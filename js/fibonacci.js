be3bc5a66f79941ef31350cd6f7977684bf9d534
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-19 16:49:37 +0300

    milestone 02 done :octocat:

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,7 +1,25 @@
-let fIndex = 13;
-let fNumber = 233;
+function fibonacci(fIndex) {
+  let n1 = 0;
+  let n2 = 1;
+  let result = 0;
 
-let result = `The Fibonacci of ${fIndex} is ${fNumber}`;
+  if (fIndex === n1) {
+    result = 0;
+  } else {
+    for (i = 1; i < fIndex; i++) {
+      result = n1 + n2;
+      n1 = n2;
+      n2 = result;
+    }
+  }
+
+  return result;
+}
+
+let inputIndex = 13;
+let fNumber = fibonacci(inputIndex);
+
+let final = `The Fibonacci of ${inputIndex} is ${fNumber}`;
 
 const fibResult = document.querySelector(".fibonacci");
-fibResult.innerText = result;
+fibResult.innerText = final;
