980c6a70d3e63390a30d4889cf95d89e73324af9
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-19 15:59:41 +0300

    initial commit + milestone 01 :octocat:

---
+++ js/fibonacci.js
@@ -0,0 +1,7 @@
+let fIndex = 13;
+let fNumber = 233;
+
+let result = `The Fibonacci of ${fIndex} is ${fNumber}`;
+
+const fibResult = document.querySelector(".fibonacci");
+fibResult.innerText = result;
