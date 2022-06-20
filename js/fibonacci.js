fa476b08eb65a67756be81c28beecd7c8e400133
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-20 13:05:19 +0300

    milestone 04 + catch input on enter :octocat:

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,24 +1,36 @@
+// constants
+
+const FIB_URL = "http://localhost:5050/fibonacci/";
+
 // page elements
 
 const button = document.getElementById("submit");
 const fibResult = document.querySelector(".fibonacci");
+const input = document.getElementById("inputNumber");
 
 // functions
 
-function fibonacci(fIndex) {
-  if (fIndex <= 1) {
-    return fIndex;
-  } else {
-    return fibonacci(fIndex - 1) + fibonacci(fIndex - 2);
-  }
-}
-
 function giveResults() {
-  const inputIndex = document.getElementById("inputNumber").value;
-  const fNumber = fibonacci(inputIndex);
-  fibResult.innerText = fNumber;
+  const inputIndex = input.value;
+
+  fetch(`${FIB_URL}${inputIndex}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const result = data.result;
+      fibResult.innerText = result;
+    });
 }
 
 // event listeners
 
 button.addEventListener("click", giveResults);
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    button.click();
+  }
+});
