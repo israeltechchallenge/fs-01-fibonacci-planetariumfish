bf388569d904ced228cca5193da0548c8165444a
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-20 17:45:24 +0300

    milestone 05 done :octocat:

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -7,20 +7,33 @@ const FIB_URL = "http://localhost:5050/fibonacci/";
 const button = document.getElementById("submit");
 const fibResult = document.querySelector(".fibonacci");
 const input = document.getElementById("inputNumber");
+const spinner = document.querySelector(".spinner");
 
 // functions
 
 function giveResults() {
   const inputIndex = input.value;
 
-  fetch(`${FIB_URL}${inputIndex}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const result = data.result;
-      fibResult.innerText = result;
+  if (inputIndex < 51) {
+    spinner.classList.add("show");
+    input.classList.remove("is-invalid");
+    fetch(`${FIB_URL}${inputIndex}`).then((response) => {
+      if (!response.ok) {
+        response.text().then((data) => {
+          spinner.classList.remove("show");
+          fibResult.innerHTML = `<span class="text-danger">Server Error: ${data}</span>`;
+        });
+      } else {
+        response.json().then((data) => {
+          spinner.classList.remove("show");
+          const result = data.result;
+          fibResult.innerHTML = `<b><u>${result}</u></b>`;
+        });
+      }
     });
+  } else {
+    input.classList.add("is-invalid");
+  }
 }
 
 // event listeners
