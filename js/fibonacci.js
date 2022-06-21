90337c248e48cff1119d1a67b7fef304d18e6302
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-21 16:01:39 +0300

    finish milestone 6 :octocat:

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -9,6 +9,8 @@ const button = document.getElementById("submit");
 const fibResult = document.querySelector(".fibonacci");
 const input = document.getElementById("inputNumber");
 const spinner = document.querySelector(".spinner");
+const mainSpinner = document.querySelector(".main-spinner");
+const history = document.getElementById("search-history");
 
 // functions
 
@@ -35,36 +37,62 @@ function giveResults() {
   } else {
     input.classList.add("is-invalid");
   }
+  refresh();
 }
 
 function fibHistory() {
   fetch(HIST_URL)
     .then((response) => {
-      response.json();
+      return response.json();
     })
     .then((data) => {
-      console.log(data);
+      displayHistory(data.results);
+    })
+    .catch((err) => {
+      history.innerText = "Fetch failed (The server is probably down)";
     });
+}
+
+function displayHistory(prevArray) {
+  prevArray.sort((a, b) => a.createdDate - b.createdDate);
+  for (let prevSearch of prevArray) {
+    history.appendChild(addSearchResult(prevSearch));
+  }
+  mainSpinner.classList.remove("show");
+}
+
+function addSearchResult(item) {
+  let sentence = document.createElement("div");
+  let date = new Date(item.createdDate);
+  let classes = ["border-bottom", "border-dark", "pb-1", "pt-2"];
+  sentence.innerHTML = `The Fibonacci of <b>${item.number}</b> is <b>${
+    item.result
+  }</b>. Calculated at: ${date.toString()}`;
+  sentence.classList.add(...classes);
+  return sentence;
+}
+
+function refresh() {
+  history.innerHTML = "";
+  fibHistory();
 }
 
 // event listeners
 
 button.addEventListener("click", giveResults);
 
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    button.click();
-  }
-});
-
 // source for the next two: https://stackoverflow.com/questions/37043867/how-to-avoid-decimal-values-in-input-type-number
 // prevent inputting or pasting decimal numbers
+// triggers button click on enter
 
 input.addEventListener("keydown", (e) => {
   if (e.key === ".") {
     e.preventDefault();
+  }
+
+  if (e.key === "Enter") {
+    e.preventDefault();
+    button.click();
   }
 });
 
@@ -74,3 +102,5 @@ input.addEventListener("paste", (e) => {
     pasteData.replace(/[^0-9]*/g, "");
   }
 });
+
+document.addEventListener("DOMContentLoaded", refresh);
