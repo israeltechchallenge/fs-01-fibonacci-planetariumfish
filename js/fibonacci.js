86182e2f29bdcd2af6d3d058ec725f4779ff968d
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-22 11:57:07 +0300

    implement sorting (milestone 7.1) :octocat:

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -12,6 +12,11 @@ const spinner = document.querySelector(".spinner");
 const mainSpinner = document.querySelector(".main-spinner");
 const history = document.getElementById("search-history");
 const check = document.getElementById("saveCalc");
+const dropdown = document.querySelector(".dropdown-menu");
+
+// state tracking variables
+
+let method = "dDesc"; // default sorting method
 
 // functions
 
@@ -54,29 +59,51 @@ async function fibHistory() {
   try {
     const response = await fetch(HIST_URL);
     const data = await response.json();
-    displayHistory(data.results);
+    const sorted = sortResults(data.results, method);
+    displayHistory(sorted);
   } catch (err) {
     history.innerText = "Fetch failed (The server is probably down)";
   }
 }
 
-function displayHistory(prevArray) {
-  prevArray.sort((a, b) => a.createdDate - b.createdDate);
-  for (let prevSearch of prevArray) {
-    history.appendChild(addSearchResult(prevSearch));
+function displayHistory(data) {
+  for (let item of data) {
+    history.appendChild(addSearchResult(item));
   }
   mainSpinner.classList.remove("show");
 }
 
 function addSearchResult(item) {
-  let sentence = document.createElement("div");
-  let date = new Date(item.createdDate);
-  let classes = ["border-bottom", "border-dark", "pb-1", "pt-2"];
+  const sentence = document.createElement("div");
+  const date = new Date(item.createdDate);
+  const classes = ["border-bottom", "border-dark", "pb-1", "pt-2"];
   sentence.innerHTML = `The Fibonacci of <b>${item.number}</b> is <b>${
     item.result
   }</b>. Calculated at: ${date.toString()}`;
   sentence.classList.add(...classes);
   return sentence;
+}
+
+function sortResults(data, method) {
+  switch (method) {
+    case "nAsc":
+      data.sort((a, b) => b.number - a.number);
+      break;
+    case "nDesc":
+      data.sort((a, b) => a.number - b.number);
+      break;
+    case "dAsc":
+      data.sort((a, b) => b.createdDate - a.createdDate);
+      break;
+    case "dDesc":
+      data.sort((a, b) => a.createdDate - b.createdDate);
+      break;
+    default:
+      data.sort((a, b) => a.createdDate - b.createdDate);
+      break;
+  }
+
+  return data;
 }
 
 function refresh() {
@@ -118,6 +145,11 @@ input.addEventListener("paste", (e) => {
   if (pasteData) {
     pasteData.replace(/[^0-9]*/g, "");
   }
+});
+
+dropdown.addEventListener("click", (e) => {
+  method = e.target.id;
+  refresh();
 });
 
 document.addEventListener("DOMContentLoaded", refresh);
